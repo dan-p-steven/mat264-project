7fd4322d127d5941b86f737f9c4fe0e66b2593be
Daniel Steven <dan.p.steven@gmail.com>
2026-03-29 13:35:32 -0400

    add cleaner jacobian function

--- main.py
+++ main.py
@@ -7,18 +7,36 @@ class InverseKinematics:
         self.l1 = l1
         self.l2 = l2
 
-# for jacobian
-    #def _dx_dt1(self, t1, t2):
-    #    return -self.l1 * np.sin(t1) - self.l2 * np.sin(t1 + t2)
-    #
-    #def _dx_dt2(self, t1, t2):
-    #    return -self.l2 * np.sin(t1 + t2)
-    #
-    #def _dy_dt1(self, t1, t2):
-    #    return self.l1 * np.cos(t1) + self.l2 * np.cos(t1 + t2)
-    #
-    #def _dy_dt2(self, t1, t2):
-    #    return self.l2 * np.cos(t1 + t2)
+    # for jacobian
+    def _dx_dt1(self, t1, t2):
+        return -self.l1 * np.sin(t1) - self.l2 * np.sin(t1 + t2)
+    
+    def _dx_dt2(self, t1, t2):
+        return -self.l2 * np.sin(t1 + t2)
+    
+    def _dy_dt1(self, t1, t2):
+        return self.l1 * np.cos(t1) + self.l2 * np.cos(t1 + t2)
+    
+    def _dy_dt2(self, t1, t2):
+        return self.l2 * np.cos(t1 + t2)
+
+    def jacobian(self, theta: np.ndarray) -> np.ndarray:
+        """
+        Compute the Jacobian of the robot arm kinematics.
+
+        Input: 
+            theta: 1x2 vector,       the angles of the two joints of the robot
+        Returns: 
+            J: 2x2 matrix,           jacobian matrix of kinematics of arm
+        """
+        t1, t2 = theta
+
+        J = np.array([
+            [-self.l1 * np.sin(t1) - self.l2 * np.sin(t1 + t2), -self.l2 * np.sin(t1 + t2)],
+            [self.l1 * np.cos(t1) + self.l2 * np.cos(t1 + t2), self.l2 * np.cos(t1 + t2)]
+            ])
+
+        return J
 
     def forward_k(self, t1, t2):
         """
@@ -29,32 +47,32 @@ class InverseKinematics:
         y = self.l1 * np.sin(t1) + self.l2 * np.sin(t1 + t2)
         return x, y
 
-    #def inverse_k(self, x, y, t1, t2) -> tuple(float, float):
-    #    """
-    #    input x and y coordinates of the end effector, initial guess of t1 and t2
-    #    return angles t1 and t2
-    #    """
-    #    X_orig = np.array([x, y])
+    def inverse_k(self, x, y, t1, t2) -> tuple(float, float):
+        """
+        input x and y coordinates of the end effector, initial guess of t1 and t2
+        return angles t1 and t2
+        """
+        X_orig = np.array([x, y])
 
-    #    t_new = np.array([t1, t2])
-    #    x_n, y_n = self.forward_k(t_new[0], t_new[1])
-    #    X_new = np.array([x_n, y_n])
+        t_new = np.array([t1, t2])
+        x_n, y_n = self.forward_k(t_new[0], t_new[1])
+        X_new = np.array([x_n, y_n])
 
-    #    while np.linalg.norm(X_new - X_orig) > TOLERENCE: # use 2-norm
+        while np.linalg.norm(X_new - X_orig) > TOLERENCE: # use 2-norm
 
-    #        # t1_n, t2_n
-    #        t = t_new
-    #        # x_n, y_n
-    #        F_n = X_new
-    #        # pseudo inverse jacobian
-    #        J = np.array([[self._dx_dt1(t[0], t[1]), self._dx_dt2(t[0], t[1])], [self._dy_dt1(t[0], t[1]), self._dy_dt2(t[0], t[1])]])
-    #        J_pinv = np.linalg.pinv(J)
+            # t1_n, t2_n
+            t = t_new
+            # x_n, y_n
+            F_n = X_new
+            # pseudo inverse jacobian
+            J = np.array([[self._dx_dt1(t[0], t[1]), self._dx_dt2(t[0], t[1])], [self._dy_dt1(t[0], t[1]), self._dy_dt2(t[0], t[1])]])
+            J_pinv = np.linalg.pinv(J)
 
-    #        # update t1_n, t2_n
-    #        t_new = t - np.linalg.matmul(J_pinv, F_n)
-    #        # update x_n, y_n
-    #        x_n, y_n = self.forward_k(t_new[0], t_new[1])
-    #        X_new = np.array([x_n, y_n])
-    #    return t_new[0], t_new[1]
+            # update t1_n, t2_n
+            t_new = t - np.linalg.matmul(J_pinv, F_n)
+            # update x_n, y_n
+            x_n, y_n = self.forward_k(t_new[0], t_new[1])
+            X_new = np.array([x_n, y_n])
+        return t_new[0], t_new[1]
 
         
